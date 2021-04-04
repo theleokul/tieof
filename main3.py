main3: remove validation error normalization for consistency with GHER DINEOF; dineof: fix progress bar

main3.py
@@ -17,6 +17,8 @@ def parse_args():
     parser.add_argument('--nitemax', type=int, default=100)
     parser.add_argument('--refit', action='store_true')
     parser.add_argument('--lat-lon-sep-centering', action='store_true')
+    parser.add_argument('--random-seed', type=int, default=2434311)
+    parser.add_argument('--val-size', type=float, default=0.045)
     args = parser.parse_args()
     return args
 
@@ -51,10 +53,10 @@ def main():
     X = np.asarray(np.nonzero(~np.isnan(tensor))).T
     y = tensor[tuple(X.T)]
 
-    np.random.seed(42)
+    np.random.seed(args.random_seed)
     biner = KBinsDiscretizer(n_bins=10, encode='ordinal', strategy='kmeans')
     stratify_y = biner.fit_transform(y[:, None]).flatten().astype(int)
-    X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.045, random_state=42, stratify=stratify_y)
+    X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=args.val_size, random_state=args.random_seed, stratify=stratify_y)
     print(f'Missing ratio: {(mask.sum() * args.tensor_shape[-1] - y.shape[0]) / (mask.sum() * args.tensor_shape[-1])}')
     print(f'Train points: {y_train.shape[0]}, val points: {y_val.shape[0]}')
 
@@ -63,7 +65,7 @@ def main():
     for R in Rs:
         d = get_model(args, R)
         d.fit(X_train, y_train)
-        val_errors.append(-d.score(X_val, y_val))
+        val_errors.append(-d.score(X_val, y_val) * y_val.std())
         print(f'Validation error: {val_errors[-1]}')
 
     best_R = Rs[np.argmin(val_errors)]

models/dineof.py
@@ -65,7 +65,7 @@ class DINEOF(BaseEstimator):
         pbar = trange(self.nitemax, desc='Reconstruction')
         conv_error = 0
         energy_per_iter = []
-        for i in range(self.nitemax):
+        for i in pbar:
             u, s, vt = svds(mat, k=self.K, tol=self.tol)
 
             # Save energy characteristics for this iteration
@@ -79,6 +79,9 @@ class DINEOF(BaseEstimator):
 
             new_conv_error = np.sqrt(np.mean(np.power(mat_hat[nan_mask] - mat[nan_mask], 2))) / mat[non_nan_mask].std()
             mat = mat_hat
+
+            pbar.set_postfix(error=new_conv_error, rel_error=abs(new_conv_error - conv_error))
+
             if (new_conv_error <= self.toliter) or (abs(new_conv_error - conv_error) < self.toliter):
                 break
             conv_error = new_conv_error