e8bb808a07d47d5de894b095080f2247d360f803
Leonid Kulikov <eduleokul@gmail.com>
2021-05-09 15:00:56 +0000

    wip

--- script/ki_cluster/dineof_aqua.sh
+++ script/ki_cluster/dineof_aqua.sh
@@ -1,7 +1,6 @@
 #! /bin/bash
 #SBATCH -D /s/ls4/users/leokul01/dineof3/script
-#SBATCH -n 1
-#SBATCH --cpus-per-task 19
+#SBATCH -n 60
 #SBATCH -o %j.out
 #SBATCH -e %j.err
 #SBATCH -t 02-23:59:59
@@ -16,7 +15,8 @@ $MPIRUN python main3_mp.py -c config/main3_default_ki_cluster.yml \
     --interpolated-stem interpolated_5kmradius_thresh2 \
     --output-stem Output_5kmradius_thresh2 \
     --decomposition-method DINEOF \
-    --early-stopping 1
+    --early-stopping 1 \
+    &>/dev/null &
 
 ### es - 5km - no thresh
 $MPIRUN python main3_mp.py -c config/main3_default_ki_cluster.yml \
@@ -25,7 +25,8 @@ $MPIRUN python main3_mp.py -c config/main3_default_ki_cluster.yml \
     --interpolated-stem interpolated_5kmradius \
     --output-stem Output_5kmradius \
     --decomposition-method DINEOF \
-    --early-stopping 1
+    --early-stopping 1 \
+    &>/dev/null &
 
 ### es - 3n - no thresh
 $MPIRUN python main3_mp.py -c config/main3_default_ki_cluster.yml \
@@ -34,4 +35,5 @@ $MPIRUN python main3_mp.py -c config/main3_default_ki_cluster.yml \
     --interpolated-stem interpolated_3neighbours \
     --output-stem Output_3neighbours \
     --decomposition-method DINEOF \
-    --early-stopping 1
+    --early-stopping 1 \
+    &>/dev/null &
