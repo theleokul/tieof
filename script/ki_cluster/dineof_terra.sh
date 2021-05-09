e32e08848b9a139d9fefdf026a9eb31217075522
Leonid Kulikov <eduleokul@gmail.com>
2021-05-09 13:34:13 +0000

    add test dineof bash scenarios for ki cluster

---
+++ script/ki_cluster/dineof_terra.sh
@@ -0,0 +1,37 @@
+#! /bin/bash
+#SBATCH -D /s/ls4/users/leokul01/dineof3/script
+#SBATCH -n 1
+#SBATCH --cpus-per-task 21
+#SBATCH -o %j.out
+#SBATCH -e %j.err
+#SBATCH -t 02-23:59:59
+#SBATCH -p hpc4-3d
+
+module load openmpi intel-compilers
+
+### es - 5km - thresh2
+$MPIRUN python main3.py -c config/main3_default.yml \
+    -S terra \
+    --logs ../test/reconstruction_logs/dineof_es_5kmradius_thresh2_terra
+    --interpolated-stem interpolated_5kmradius_thresh2 \
+    --output-stem Output_5kmradius_thresh2 \
+    --decomposition-method DINEOF
+    --early-stopping 1
+
+### es - 5km - no thresh
+$MPIRUN python main3.py -c config/main3_default.yml \
+    -S terra \
+    --logs ../test/reconstruction_logs/dineof_es_5kmradius_terra
+    --interpolated-stem interpolated_5kmradius \
+    --output-stem Output_5kmradius \
+    --decomposition-method DINEOF
+    --early-stopping 1
+
+### es - 3n - no thresh
+$MPIRUN python main3.py -c config/main3_default.yml \
+    -S terra \
+    --logs ../test/reconstruction_logs/dineof_es_3neighbours_terra
+    --interpolated-stem interpolated_3neighbours \
+    --output-stem Output_3neighbours \
+    --decomposition-method DINEOF
+    --early-stopping 1
