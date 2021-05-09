84161cae4103fc18b28b2b31947fdbf94928b03f
Leonid Kulikov <eduleokul@gmail.com>
2021-05-09 19:06:06 +0000

    script/ki_cluster/parafac/es: add scripts for parafac

---
+++ script/ki_cluster/parafac/es/3n/no_thresh/seawifs.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+#SBATCH -D /s/ls4/users/leokul01/dineof3/script
+#SBATCH --ntasks 1
+#SBATCH --cpus-per-task 48
+#SBATCH -o %j.out
+#SBATCH -e %j.err
+#SBATCH -t 02-23:59:59
+#SBATCH -p hpc4-3d
+
+module load openmpi intel-compilers
+export OPENBLAS_NUM_THREADS=8
+
+
+### es - 3n - no thresh
+$MPIRUN python main3_mp.py -c config/main3_default_ki_cluster.yml \
+    -S seawifs \
+    --logs ../test/reconstruction_logs/dineof_es_3neighbours_seawifs \
+    --interpolated-stem interpolated_3neighbours \
+    --output-stem Output_3neighbours \
+    --decomposition-method PARAFAC \
+    --early-stopping 1
---
+++ script/ki_cluster/parafac/es/5km/no_thresh/terra.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+#SBATCH -D /s/ls4/users/leokul01/dineof3/script
+#SBATCH --ntasks 1
+#SBATCH --cpus-per-task 48
+#SBATCH -o %j.out
+#SBATCH -e %j.err
+#SBATCH -t 02-23:59:59
+#SBATCH -p hpc4-3d
+
+module load openmpi intel-compilers
+export OPENBLAS_NUM_THREADS=8
+
+### es - 5km - no thresh
+$MPIRUN python main3_mp.py -c config/main3_default_ki_cluster.yml \
+    -S terra \
+    --logs ../test/reconstruction_logs/dineof_es_5kmradius_terra \
+    --interpolated-stem interpolated_5kmradius \
+    --output-stem Output_5kmradius \
+    --decomposition-method PARAFAC \
+    --early-stopping 1
