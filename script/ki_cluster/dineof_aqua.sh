20da37211267e7a8e19c4a4c6876e47382c9bc83
Leonid Kulikov <eduleokul@gmail.com>
2021-05-09 13:39:53 +0000

    wip

--- script/ki_cluster/dineof_aqua.sh
+++ script/ki_cluster/dineof_aqua.sh
@@ -12,26 +12,26 @@ module load openmpi intel-compilers
 ### es - 5km - thresh2
 $MPIRUN python main3.py -c config/main3_default.yml \
     -S aqua \
-    --logs ../test/reconstruction_logs/dineof_es_5kmradius_thresh2_aqua
+    --logs ../test/reconstruction_logs/dineof_es_5kmradius_thresh2_aqua \
     --interpolated-stem interpolated_5kmradius_thresh2 \
     --output-stem Output_5kmradius_thresh2 \
-    --decomposition-method DINEOF
+    --decomposition-method DINEOF \
     --early-stopping 1
 
 ### es - 5km - no thresh
 $MPIRUN python main3.py -c config/main3_default.yml \
     -S aqua \
-    --logs ../test/reconstruction_logs/dineof_es_5kmradius_aqua
+    --logs ../test/reconstruction_logs/dineof_es_5kmradius_aqua \
     --interpolated-stem interpolated_5kmradius \
     --output-stem Output_5kmradius \
-    --decomposition-method DINEOF
+    --decomposition-method DINEOF \
     --early-stopping 1
 
 ### es - 3n - no thresh
 $MPIRUN python main3.py -c config/main3_default.yml \
     -S aqua \
-    --logs ../test/reconstruction_logs/dineof_es_3neighbours_aqua
+    --logs ../test/reconstruction_logs/dineof_es_3neighbours_aqua \
     --interpolated-stem interpolated_3neighbours \
     --output-stem Output_3neighbours \
-    --decomposition-method DINEOF
+    --decomposition-method DINEOF \
     --early-stopping 1
--- script/ki_cluster/dineof_terra.sh
+++ script/ki_cluster/dineof_terra.sh
@@ -12,26 +12,26 @@ module load openmpi intel-compilers
 ### es - 5km - thresh2
 $MPIRUN python main3.py -c config/main3_default.yml \
     -S terra \
-    --logs ../test/reconstruction_logs/dineof_es_5kmradius_thresh2_terra
+    --logs ../test/reconstruction_logs/dineof_es_5kmradius_thresh2_terra \
     --interpolated-stem interpolated_5kmradius_thresh2 \
     --output-stem Output_5kmradius_thresh2 \
-    --decomposition-method DINEOF
+    --decomposition-method DINEOF \
     --early-stopping 1
 
 ### es - 5km - no thresh
 $MPIRUN python main3.py -c config/main3_default.yml \
     -S terra \
-    --logs ../test/reconstruction_logs/dineof_es_5kmradius_terra
+    --logs ../test/reconstruction_logs/dineof_es_5kmradius_terra \
     --interpolated-stem interpolated_5kmradius \
     --output-stem Output_5kmradius \
-    --decomposition-method DINEOF
+    --decomposition-method DINEOF \
     --early-stopping 1
 
 ### es - 3n - no thresh
 $MPIRUN python main3.py -c config/main3_default.yml \
     -S terra \
-    --logs ../test/reconstruction_logs/dineof_es_3neighbours_terra
+    --logs ../test/reconstruction_logs/dineof_es_3neighbours_terra \
     --interpolated-stem interpolated_3neighbours \
     --output-stem Output_3neighbours \
-    --decomposition-method DINEOF
+    --decomposition-method DINEOF \
     --early-stopping 1
