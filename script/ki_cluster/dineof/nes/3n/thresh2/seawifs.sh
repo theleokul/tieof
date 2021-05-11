48ee820387fc518954ead445502b4e99562f4857
Leonid Kulikov <eduleokul@gmail.com>
2021-05-11 07:44:00 +0000

    script/ki_cluster: add scripts for 3n_thresh2 interpolation

---
+++ script/ki_cluster/dineof/nes/3n/thresh2/seawifs.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+#SBATCH -D /s/ls4/users/leokul01/dineof3/script
+#SBATCH --ntasks 1
+#SBATCH --cpus-per-task 48
+#SBATCH -o %j.out
+#SBATCH -e %j.err
+#SBATCH -t 02-23:59:59
+#SBATCH -p hpc4-3d
+
+module load openmpi intel-compilers
+export OPENBLAS_NUM_THREADS=8
+
+
+### es - 3n - no thresh
+$MPIRUN python main3_mp.py -c config/main3_default_ki_cluster.yml \
+    -S seawifs \
+    --logs ../test/reconstruction_logs/dineof_nes_3neighbours_thresh2_seawifs \
+    --interpolated-stem interpolated_3neighbours_thresh2 \
+    --output-stem Output_3neighbours_thresh2 \
+    --decomposition-method DINEOF \
+    --early-stopping 0
